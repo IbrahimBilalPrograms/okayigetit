cfcfc511c4d19c0a78c9308cf316bd4eb2fba89c
epicgamer <68295724+IbrahimBilalPrograms@users.noreply.github.com>
2020-12-26 09:45:21 +0300

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -105,8 +105,8 @@ const Footer = () => {
       </StyledSocialLinks>
 
       <StyledCredit tabindex="-1">
-        <a href="https://github.com/bchiang7/v4">
-          <div>Designed &amp; Built by Brittany Chiang</div>
+   
+          <div>Designed &amp; Built by Ibrahim Bilal</div>
 
           {githubInfo.stars && githubInfo.forks && (
             <div className="github-stats">
