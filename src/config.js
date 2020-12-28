447354109e3fb93353995d92db82bde7245b2715
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2020-12-28 08:37:51 +0300

    e

--- src/config.js
+++ src/config.js
@@ -4,23 +4,23 @@ module.exports = {
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/ok',
+      url: 'https://github.com/efererer',
     },
     {
       name: 'Instagram',
-      url: 'https://www.instagram.com/',
+      url: 'https://www.instagram.com/dudedededer',
     },
     {
       name: 'Twitter',
-      url: 'https://twitter.com/',
+      url: 'https://twitter.com/nololzerzek',
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/',
+      url: 'https://www.linkedin.com/in/workinmek',
     },
     {
       name: 'Codepen',
-      url: 'https://codepen.io/',
+      url: 'https://codepen.io/ddddddd',
     },
   ],
 
